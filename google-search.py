2e070ccf50941a548c9fe7352e7aa7e2e96b7100
0xPr0xy <0xPr0xy@gmail.com>
2012-08-21 18:28:05 +0200

    .

--- google-search.py
+++ google-search.py
@@ -3,10 +3,9 @@ import requests
 
 for url in search("inurl:\"passes\" OR inurl:\"passwords\" OR inurl:\"credentials\" -search -download -techsupt -git -games -gz -bypass -exe filetype:txt @yahoo.com OR @gmail OR @hotmail OR @rediff", stop=20):
 	try:
-		r = requests.get(url)
-		parts = url.split('/')
-		filename = parts[-1]
+		filename = url.split('/')[-1]
 		print 'writing %s...' %filename
+		r = requests.get(url)
 		with open('%s' % filename, 'wb')  as code:
 			code.write(r.content)
 		print 'done'
